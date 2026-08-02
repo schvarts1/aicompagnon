698d26ca3795e803f7bbbf93ee101e52b0198db0
schvarts1 <schvarts11@gmail.com>
2026-08-02 02:44:39 +0200

    INIT

---
+++ src/main/java/git/schvarts11/aicompagnon/AicompagnonModBootstrap.java
@@ -0,0 +1,27 @@
+/*
+ *	MCreator note:
+ *
+ *	If you lock base mod element files, you can edit this file and the proxy files
+ *	and they won't get overwritten. If you change your mod package or modid, you
+ *	need to apply these changes to this file MANUALLY.
+ *
+ *
+ *	If you do not lock base mod element files in Workspace settings, this file
+ *	will be REGENERATED on each build.
+ *
+ */
+package git.schvarts11.aicompagnon;
+
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+import io.papermc.paper.plugin.bootstrap.BootstrapContext;
+
+@SuppressWarnings("UnstableApiUsage")
+public class AicompagnonModBootstrap implements PluginBootstrap {
+
+	@Override
+	public void bootstrap(BootstrapContext context) {
+		LifecycleEventManager<BootstrapContext> lifecycle = context.getLifecycleManager();
+
+	}
+}
